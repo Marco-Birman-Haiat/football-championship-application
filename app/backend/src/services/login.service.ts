a4f8add2b223ac95f446a7d7d910ddd33fc09df3
Marco Haiat <marco.haiat@gmail.com>
2023-06-14 18:47:25 -0300

    feat: login validations and fix at bcrypt compare

--- app/backend/src/services/login.service.ts
+++ app/backend/src/services/login.service.ts
@@ -1,9 +1,10 @@
 import * as bcrypt from 'bcryptjs';
 
-import { ServiceResponse } from '../Interfaces/serviceResponse';
+import { ServiceResponse } from '../Interfaces/responses/serviceResponse';
 import { Login } from '../Interfaces/entities/userEntity';
 import UserRepository from '../repositories/userRepository';
 import { IJwtAuthorization } from '../utils/jwtFunctions';
+import validateLogin from './validations/validateLogin';
 
 export interface ILoginService {
   login(userData: Login): Promise<ServiceResponse<{ token: string }>>;
@@ -16,9 +17,12 @@ export default class LoginService implements ILoginService {
   ) {}
 
   async login(userData: Login): Promise<ServiceResponse<{ token: string; }>> {
+    const error = validateLogin.executeValidation(userData);
+    if (error.status) return error;
+
     const foundUser = await this.userRepository.findByEmail(userData.email);
 
-    if (!foundUser || bcrypt.compareSync(userData.password, foundUser.password)) {
+    if (!foundUser || !bcrypt.compareSync(userData.password, foundUser.password)) {
       return { status: 'UNAUTHORIZED', data: { message: 'email and/or password incorrect' } };
     }
     const tokenPayload = { id: foundUser.id, email: foundUser.email };
