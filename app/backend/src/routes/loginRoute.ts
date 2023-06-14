bae83034de6fa639e30a742be8b0f01a61e0229c
Marco Haiat <marco.haiat@gmail.com>
2023-06-14 18:47:50 -0300

    feat: login middleware inserted at loginRoute

--- app/backend/src/routes/loginRoute.ts
+++ app/backend/src/routes/loginRoute.ts
@@ -4,6 +4,7 @@ import LoginController from '../controllers/login.controller';
 import LoginService from '../services/login.service';
 import UserRepository from '../repositories/userRepository';
 import JwtAuthorization from '../utils/jwtFunctions';
+import ValidateRequest from '../middleware/requestValidation';
 
 const loginRoute = Router();
 
@@ -12,6 +13,10 @@ const jtwUtils = new JwtAuthorization();
 const loginService = new LoginService(userRepository, jtwUtils);
 const loginController = new LoginController(loginService);
 
-loginRoute.post('/', async (req, res) => loginController.login(req, res));
+loginRoute.post(
+  '/',
+  ValidateRequest.validateLogin,
+  async (req, res) => loginController.login(req, res),
+);
 
 export default loginRoute;
