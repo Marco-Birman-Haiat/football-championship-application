4f477f7aa8d1bc493309657ad0f24be6db1ad932
Marco Haiat <marco.haiat@gmail.com>
2023-06-15 10:45:46 -0300

    feat: get login/role implemented controller/route

--- app/backend/src/controllers/login.controller.ts
+++ app/backend/src/controllers/login.controller.ts
@@ -1,19 +1,43 @@
 import { Request, Response } from 'express';
 
-import LoginService from '../services/login.service';
+import { ILoginService } from '../services/login.service';
+import { AuthenticatedRequest } from '../middleware/tokenMiddleware';
+import { IUserService } from '../services/user.service';
+import getErrorCode from '../utils/httpError';
 
 export interface ILoginController {
   login(req: Request, res: Response): Promise<Response>;
+  getRoleById(req: Request, res: Response): Promise<Response>;
 }
 
 export default class LoginController implements ILoginController {
-  constructor(private loginService: LoginService) {}
+  constructor(
+    private loginService: ILoginService,
+    private userService: IUserService,
+  ) {}
 
   async login(req: Request, res: Response): Promise<Response> {
     const { email, password } = req.body;
 
     const token = await this.loginService.login({ email, password });
 
+    if (token.status === 'unauthorized') {
+      return res.status(getErrorCode(token.status)).json(token.data);
+    }
     return res.status(200).json(token.data);
+  }
+
+  async getRoleById(req: Request, res: Response): Promise<Response> {
+    const { id } = (req as AuthenticatedRequest).payload;
+
+    console.log('req no controller ', (req as AuthenticatedRequest).payload, id);
+
+    const foundRole = await this.userService.getRoleById(id.toString());
+
+    if (foundRole.status === 'notFound') {
+      return res.status(getErrorCode(foundRole.status)).json(foundRole.data);
+    }
+    console.log('passou do erro controller');
+    return res.status(200).json(foundRole.data);
   }
 }
--- app/backend/src/routes/loginRoute.ts
+++ app/backend/src/routes/loginRoute.ts
@@ -3,20 +3,28 @@ import { Router } from 'express';
 import LoginController from '../controllers/login.controller';
 import LoginService from '../services/login.service';
 import UserRepository from '../repositories/userRepository';
-import JwtAuthorization from '../utils/jwtFunctions';
 import ValidateRequest from '../middleware/requestValidation';
+import TokenValidation from '../middleware/tokenMiddleware';
+import UserService from '../services/user.service';
 
 const loginRoute = Router();
 
 const userRepository = new UserRepository();
-const jtwUtils = new JwtAuthorization();
-const loginService = new LoginService(userRepository, jtwUtils);
-const loginController = new LoginController(loginService);
+const userService = new UserService(userRepository);
+
+const loginService = new LoginService(userRepository);
+const loginController = new LoginController(loginService, userService);
 
 loginRoute.post(
   '/',
   ValidateRequest.validateLogin,
   async (req, res) => loginController.login(req, res),
+);
+
+loginRoute.get(
+  '/role',
+  TokenValidation.validateToken,
+  async (req, res) => loginController.getRoleById(req, res),
 );
 
 export default loginRoute;
