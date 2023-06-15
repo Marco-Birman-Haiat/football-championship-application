3b9a4cfba6fca28097ea5f556c5fb93a82ff8ff1
Marco Haiat <marco.haiat@gmail.com>
2023-06-15 12:14:06 -0300

    feat: login and user mock

---
+++ app/backend/src/tests/mocks/login.mock.ts
@@ -0,0 +1,9 @@
+export const validLoginBody = {
+  email: 'admin@admin.com',
+  password: 'secret_admin'
+};
+
+export const InvalidLoginBody = {
+  email: '@admin.com',
+  password: '12345'
+};
---
+++ app/backend/src/tests/mocks/user.mock.ts
@@ -0,0 +1,9 @@
+import { UserEntity } from "../../Interfaces/entities/userEntity";
+
+export const registeredUser = {
+  id: 1,
+  username: 'Test Name',
+  email: 'admin@admin.com',
+  password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW',
+  role: 'admin',
+}
