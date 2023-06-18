1c200a7f5b483effde3c7f0d0a86a30848c5d59d
Marco Haiat <marco.haiat@gmail.com>
2023-06-18 16:07:04 -0300

    feat: match route

--- app/backend/src/routes/index.ts
+++ app/backend/src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import teamRoute from './teamRoute';
 import loginRoute from './loginRoute';
+import matchRoute from './matchRoute';
 
 const router = Router();
 
 router.use('/teams', teamRoute);
 router.use('/login', loginRoute);
+router.use('/matches', matchRoute);
 
 export default router;
---
+++ app/backend/src/routes/matchRoute.ts
@@ -0,0 +1,20 @@
+import * as express from 'express';
+import MatchController from '../controllers/match.controller';
+import MatchService from '../services/match.service';
+import MatchRepository from '../repositories/matchRepository';
+import TokenValidation from '../middleware/tokenMiddleware';
+
+const matchRoute = express.Router();
+
+const matchRepository = new MatchRepository();
+const matchService = new MatchService(matchRepository);
+const matchController = new MatchController(matchService);
+
+matchRoute.get('/', async (req, res) => matchController.getMatches(req, res));
+matchRoute.patch(
+  '/:id/finish',
+  TokenValidation.validateToken,
+  async (req, res) => matchController.finishMatch(req, res),
+);
+
+export default matchRoute;
