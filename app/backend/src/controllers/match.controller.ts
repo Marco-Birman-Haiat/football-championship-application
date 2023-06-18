f1b774c4ee7d2fb30d3e67cde76a18aadfa9a707
Marco Haiat <marco.haiat@gmail.com>
2023-06-18 16:06:33 -0300

    feat: match controller

---
+++ app/backend/src/controllers/match.controller.ts
@@ -0,0 +1,51 @@
+import { Request, Response } from 'express';
+import { IMatchService } from '../services/match.service';
+import getErrorCode from '../utils/httpError';
+
+export interface IMatchController {
+  getMatches(req: Request, res: Response): void;
+  getAll(req: Request, res: Response): Promise<Response>;
+  finishMatch(req: Request, res: Response): Promise<Response>;
+  getByInProgress(req: Request, res: Response): Promise<Response>;
+}
+
+export default class MatchController implements IMatchController {
+  constructor(private matchService: IMatchService) {}
+
+  async getMatches(req: Request, res: Response): Promise<void> {
+    const { inProgress } = req.query;
+
+    if (inProgress === undefined) {
+      this.getAll(req, res);
+    } else {
+      this.getByInProgress(req, res);
+    }
+  }
+
+  async getAll(req: Request, res: Response): Promise<Response> {
+    const allMatches = await this.matchService.getAll();
+
+    return res.status(200).json(allMatches.data);
+  }
+
+  async getByInProgress(req: Request, res: Response): Promise<Response> {
+    const { inProgress } = req.query;
+
+    const inProgressAdjusted = inProgress === 'true';
+
+    const filteredMatches = await this.matchService
+      .getByInProgress(inProgressAdjusted);
+
+    return res.status(200).json(filteredMatches.data);
+  }
+
+  async finishMatch(req: Request, res: Response): Promise<Response> {
+    const { id } = req.params;
+    const finishedMatch = await this.matchService.finishMatch(id);
+
+    if (finishedMatch.status === 'notFound') {
+      return res.status(getErrorCode(finishedMatch.status)).json(finishedMatch.data);
+    }
+    return res.status(200).json(finishedMatch.data);
+  }
+}
