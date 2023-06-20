5222a5aba8ed42132f77c7e09c3388581f56ef25
Marco Haiat <marco.haiat@gmail.com>
2023-06-20 10:57:34 -0300

    feat: match service create and updateScoreboard

--- app/backend/src/services/match.service.ts
+++ app/backend/src/services/match.service.ts
@@ -1,11 +1,15 @@
 import { IMatchRepository } from '../repositories/matchRepository';
-import { MatchEntity, MatchEntitySimple } from '../Interfaces/entities/matchEntity';
+import { MatchComponents, MatchEntity, MatchEntitySimple,
+  MatchScoreboard } from '../Interfaces/entities/matchEntity';
 import { ServiceResponse } from '../Interfaces/responses/serviceResponse';
 
 export interface IMatchService {
   getAll(): Promise<ServiceResponse<MatchEntitySimple[]>>;
   getByInProgress(inProgress: boolean): Promise<ServiceResponse<MatchEntity[]>>;
+  create(match: MatchComponents): Promise<ServiceResponse<MatchEntity>>;
   finishMatch(id: string): Promise<ServiceResponse<{ message: string }>>;
+  updateScoreboard(scoreBoard: MatchScoreboard,
+    id: string): Promise<ServiceResponse<{ message: string }>>;
 }
 
 export default class MatchService implements IMatchService {
@@ -23,6 +27,35 @@ export default class MatchService implements IMatchService {
     const filteredMatches = await this.matchRepository.getWithFilter(filterAttribute);
 
     return { status: 'ok', data: filteredMatches };
+  }
+
+  async create(match: MatchComponents): Promise<ServiceResponse<MatchEntity>> {
+    if (match.awayTeamId === match.homeTeamId) {
+      return {
+        status: 'uprocessableData',
+        data: { message: 'It is not possible to create a match with two equal teams' },
+      };
+    }
+
+    const matchToCreate = {
+      ...match,
+      inProgress: true,
+    };
+
+    try {
+      const createdMatch = await this.matchRepository.create(matchToCreate);
+      return { status: 'ok', data: createdMatch };
+    } catch (error) {
+      return { status: 'notFound', data: { message: 'There is no team with such id!' } };
+    }
+  }
+
+  async updateScoreboard(scoreBoard: MatchScoreboard, id: string):
+  Promise<ServiceResponse<{ message: string }>> {
+    const filterObject = { id: Number(id) };
+    await this.matchRepository.update(scoreBoard, filterObject);
+
+    return { status: 'ok', data: { message: 'Scoreboard updated' } };
   }
 
   async finishMatch(id: string): Promise<ServiceResponse<{ message: string }>> {
