06c080e55304e1197f484b1963dfd0585fabe642
Marco Haiat <marco.haiat@gmail.com>
2023-06-20 10:59:01 -0300

    feat: leaderboard interfaces

---
+++ app/backend/src/Interfaces/entities/teamScoreboardEntity.ts
@@ -0,0 +1,14 @@
+export interface TeamScoreboard {
+  name: string,
+  totalPoints: number,
+  totalGames: number,
+  totalVictories: number,
+  totalDraws: number,
+  totalLosses: number,
+  goalsFavor: number,
+  goalsOwn: number,
+  goalsBalance: number,
+  efficiency: number,
+}
+
+export type LeaderBoard = TeamScoreboard[];
