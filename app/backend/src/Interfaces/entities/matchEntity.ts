0ef8e98b7fd8fd091c81b289e8b033e3e09af7b7
Marco Haiat <marco.haiat@gmail.com>
2023-06-20 10:56:07 -0300

    feat: match entity segragated

--- app/backend/src/Interfaces/entities/matchEntity.ts
+++ app/backend/src/Interfaces/entities/matchEntity.ts
@@ -1,22 +1,26 @@
 import { Identifiable } from '..';
 import { TeamEntity } from './teamEntity';
 
-export interface MatchEntity extends Identifiable {
-  homeTeamId: number;
-  homeTeamGoals: number;
-  awayTeamId: number;
-  awayTeamGoals: number;
+export interface MatchEntity extends Identifiable, MatchComponents {
   inProgress: boolean;
   homeTeam?: TeamEntity;
   awayTeam?: TeamEntity;
 }
 
-export interface MatchEntitySimple extends Identifiable {
-  homeTeamId: number;
-  homeTeamGoals: number;
-  awayTeamId: number;
-  awayTeamGoals: number;
+export interface MatchEntitySimple extends Identifiable, MatchComponents {
   inProgress: boolean;
   homeTeam: { teamName: string };
   awayTeam: { teamName: string };
+}
+
+export interface MatchComponents extends MatchTeams, MatchScoreboard {}
+
+export interface MatchTeams {
+  homeTeamId: number;
+  awayTeamId: number;
+}
+
+export interface MatchScoreboard {
+  homeTeamGoals: number;
+  awayTeamGoals: number;
 }
